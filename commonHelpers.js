cdc88983d19c81ad4b58d65864220b4ee428199d
Miteos1986 <119668790+Miteos1986@users.noreply.github.com>
2024-04-12 16:04:41 +0000

    Deploying to gh-pages from @ Miteos1986/goit-js-hw-11@83d1f03a1411c496bf0277891f8880d36c3c1f40 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,16 +1,16 @@
-import{S as l}from"./assets/vendor-5af972a3.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const o of s.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function a(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(e){if(e.ep)return;e.ep=!0;const s=a(e);fetch(e.href,s)}})();const n="https://pixabay.com/api/",u="43311498-bd26fa1e6ae7e25f586fd794c",p=new URLSearchParams({key:u,q:"",image_type:"photo",orientation:"horizontal",safesearch:"true"});fetch(`${n}?${p}`).then(t=>{if(!t.ok)throw new Error(t.status);return console.log("ok"),t.json()}).then(t=>console.log(t)).catch(t=>console.log(t));const f=document.querySelector(".js-list");f.insertAdjacentHTML("beforeend",d);new l(".gallery a",{sourceAttr:"href",captionsData:"title",captionPosition:"bottom",captionDelay:200});refresh();function d(t){return t.map(({webformatURL:r,largeImageURL:a,tags:i,likes:e,views:s,comments:o,downloads:c})=>`
-<li class="gallery-item">
-<a class="gallery-link" href="${r}" >
-<img class="gallery-img" src="${a}" alt=${i} width=360 />
+import{i as l,S as u}from"./assets/vendor-0fc460d7.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();function p(r){return r.map(({webformatURL:s,largeImageURL:o,tags:i,likes:e,views:t,comments:a,downloads:c})=>`
+<li data-id=${id} class="gallery-item">
+<a class="gallery-link" href="${o}" >
+<img class="gallery-img" src="${s}" alt=${i} width=360 />
 </a>
-<ul class="gallary">
+<ul class="gallery">
 <li class="subscribe"><span class="span-subscribe">likes</span> ${e}</li>
-<li class="subscribe"><span class="span-subscribe">views</span> ${s}</li>
-<li class="subscribe"><span class="span-subscribe">comments</span> ${o}</li>
+<li class="subscribe"><span class="span-subscribe">views</span> ${t}</li>
+<li class="subscribe"><span class="span-subscribe">comments</span> ${a}</li>
 <li class="subscribe"><span class="span-subscribe">downloads</span> ${c}</li>
 </ul> 
 </li>
 
 
-    `).join("")}
+    `).join("")}const f="https://pixabay.com/api/",m="43311498-bd26fa1e6ae7e25f586fd794c",d=new URLSearchParams({key:m,q:"searchImg",image_type:"photo",orientation:"horizontal",safesearch:"true"}),h=`${f}?${d}`;function b(r){fetch(h).then(s=>{if(!s.ok)throw new Error(s.status);return console.log("ok"),s.json()}).then(s=>s.hits).catch(s=>l.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}))}const y=document.querySelector(".js-list"),n=document.querySelector(".js-search-form");n.addEventListener("submit",g);function g(r){r.preventDefault(),n.elements.input.value,b().then(s=>y.insertAdjacentHTML("beforeend",p(s.hits)))}new u(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});
 //# sourceMappingURL=commonHelpers.js.map
